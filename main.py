5e20349039cbd32e07d246a20ea901d4ec18670e
anf692 <fassane348@gmail.com>
2026-01-13 21:12:06 +0000

    ajout de fonction consulter solde et acheter credit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 solde=5000
+historiques=[]
 
 #fonction pour la saisie du code ussd
 def Code_ussd():
@@ -37,3 +38,33 @@ def confirmation(message):
             return False
         else:
             print("Incorrect")
+
+#fonction pour afficher le solde
+def Consulter_solde():
+    global solde
+    print(f"\nVotre solde actuel est egale a : {solde} FCFA")
+
+#fonction pour l'achat de credit
+def Acheter_crédit():
+    global solde
+    
+    while True:
+        montant_saisie= input("Entrez le montant a acheter: ")
+        
+        if montant_saisie.replace(" ", "").isnumeric():
+            
+            montant= int(montant_saisie)
+            if solde < montant:
+                print("Solde insuffisant")
+                return
+        
+            #verification et appel a la fonction confirmation pour voir s'il a 'confirmer' ou 'annuler'
+            if confirmation(f"Voulez-vous  achater {montant} de credit? "):
+                solde -= montant
+                #ajout de l'action dans liste des historiques
+                historiques.append(f"Vous avez acheter {montant} FCFA de credit")
+                print(f"\nFelication!!! compte recharger. Votre nouveux solde est {solde} FCFA ")
+            return
+        
+        else:
+            print("Montant incorrect!!!")
